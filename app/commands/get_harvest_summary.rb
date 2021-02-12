bfc42cab0655689c6160da83bb8af4a7d61cfd18
Agustin Gomez <agomezcampero@gmail.com>
2021-02-12 15:08:32 -0300

    feat(summary): create harvest summary

---
+++ app/commands/get_harvest_summary.rb
@@ -0,0 +1,52 @@
+class GetHarvestSummary < PowerTypes::Command.new(:client_id, :project_id)
+  NUMBER_OF_MONTHS = 2
+
+  def perform
+    {
+      client: client,
+      hours_by_month: hours_by_month
+    }
+  end
+
+  private
+
+  def entries
+    @entries ||= harvest_service.get_time_entries(client_id: @client_id,
+                                                  project_id: @project_id,
+                                                  from: from,
+                                                  to: Time.zone.today)
+  end
+
+  def client
+    return '' if entries.empty?
+
+    entries[0].attributes['client']['name']
+  end
+
+  def hours_by_month
+    init_hours_by_month
+    entries.each do |entry|
+      cur_month = get_month(entry.attributes['spent_date'])
+      @hours_by_month[cur_month] += entry.attributes['hours']
+    end
+    @hours_by_month
+  end
+
+  def init_hours_by_month
+    @hours_by_month = {}
+    (NUMBER_OF_MONTHS + 1).times { |i| @hours_by_month[get_month(Time.zone.today - i.months)] = 0 }
+  end
+
+  def get_month(date)
+    date = Time.zone.parse(date) if date.is_a?(String)
+    date.strftime("%b-%Y")
+  end
+
+  def from
+    @from ||= (Time.zone.today - NUMBER_OF_MONTHS.months).at_beginning_of_month
+  end
+
+  def harvest_service
+    @harvest_service ||= HarvestService.new
+  end
+end
